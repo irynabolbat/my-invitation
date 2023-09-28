f194eb0567bde14e3f4b7f6c5cef5b2dd66bf854
Iryna <irinabolbat98@gmail.com>
2023-09-28 18:21:55 +0300

    Updates

--- src/components/Activity/Activity.jsx
+++
@@ -1,18 +0,0 @@
-import './Activity.scss';
-
-export const Activity = () => {
-  return (
-    <div className='Activity'>
-      <div className='Activity__Circle'>
-        🎲
-      </div>
-
-      <div className='Activity__Container'>
-        <h1 className='Activity__Title'>Розваги</h1>
-
-        <p>Дресс-коду немає, але треба ж чимось вас зайняти😅</p>
-        <p>Чекаю на конкурси від вас😈</p>
-      </div>
-    </div>
-  )
-}
--- src/components/DressCode/DressCode.jsx
+++
@@ -1,20 +0,0 @@
-import './DressCode.scss';
-
-export const DressCode = () => {
-
-  return (
-    <div className='DressCode'>
-      <div className='DressCode__Circle'>
-        👗
-      </div>
-
-      <div className='DressCode__Container'>
-        <h1 className='DressCode__Title'>Дресс-код</h1>
-
-        <p>Я так і не вирішила який дресс-код хочу😁</p>
-        <p>І нічого загального крім собак та "Кохання на виживання" у нас нема😅</p>
-        <p>Тож, жодного дресс-коду😎</p>
-      </div>
-    </div>
-  );
-};
